f85c5441aaf074b3cafef275a593c4432d06917c
sppatil2 <sppatil2@illinois.edu>
2019-02-26 18:56:21 -0600

    creates num_circuits after fetching descriptors from remote

--- circuits.py
+++ circuits.py
@@ -1,22 +1,40 @@
 from stem import CircStatus
 from stem.control import Controller
+import stem.descriptor.remote
+import random
 
 hop1 = "C3777E3970FAC2C0CB2C4E166745A77650131304"
 hop2 = "749EF4A434DFD00DAB31E93DE86233FB916D31E3"
 exit = "8CF987FF43FB7F3D9AA4C4F3D96FFDF247A9A6C2"
 relays = [hop1, hop2, exit]
+num_circuits = 10
+
+fingerprints = []
+try:
+  for desc in stem.descriptor.remote.get_consensus():
+    fingerprints.append(desc.fingerprint)
+except Exception as exc:
+  print("Unable to retrieve the consensus: %s" % exc)
+
+random_middle = random.sample(fingerprints, num_circuits)
+print(random_middle)
+
+'''
+with Controller.from_port(port = 9151) as controller:
+  controller.authenticate()
+
+  for desc in controller.get_network_statuses():
+    print("found relay %s (%s)" % (desc.nickname, desc.fingerprint))
+'''
 
 # port 9151 is the tor browser port
-with Controller.from_port(port = 9051) as controller:
-    controller.authenticate() # we can look into authenticate if need be
+with Controller.from_port(port = 9151) as controller:
+    controller.authenticate() 
+    circuit_id = []
+    for relay in random_middle:
+        relays = [hop1, relay, exit]
+        circuit_id.append(controller.new_circuit(relays, await_build = True))
 
-    # should be able to build the circuits, TODO: streams if needed
-    circuit_id = controller.new_circuit(relays, await_build = True)
-    # builds the circuit we want with the set of relays specified and waits till the circuit is ready
-
-    # look at destroy(circId)
-
-    print(controller.get_circuit(circuit_id))
     for circ in sorted(controller.get_circuits()):
         if circ.status != CircStatus.BUILT:
           continue
